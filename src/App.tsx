3cf5256df94a55c7413b4544b536ee2f2ca49f03
baykalgurbuz <ebaykalgurbuz@gmail.com>
2023-12-13 23:55:38 +0300

    Heros and LibraryService components added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-
 import './App.css';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layout/HomePage/ExploreTopBooks';
+import { Carousel } from './layout/HomePage/Carousel';
+import { Heros } from './layout/HomePage/Heros';
+import { LibraryServices } from './layout/HomePage/LibraryServices';
+ 
 
 function App() {
   return (
     <div>
     <Navbar/>
     <ExploreTopBooks/>
+    <Carousel/>
+    <Heros/>
+    <LibraryServices/>
     </div>
   );
 }
