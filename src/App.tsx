d9f0b549bac1c86086a43a75c28491508769374e
baykalgurbuz <ebaykalgurbuz@gmail.com>
2023-12-13 22:25:18 +0300

    Create Carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './App.css';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layout/HomePage/ExploreTopBooks';
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <ExploreTopBooks/>
+    </div>
   );
 }
 
